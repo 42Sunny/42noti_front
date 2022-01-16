df1eaa1dee5e2b0c423a92fa181673d501415351
dopaming <over_ray@naver.com>
2022-01-16 22:10:45 +0900

    MTUP-65 footer intra 아이디 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Footer = () => {
-  return <StyledFooter>v.1 copyright 42</StyledFooter>;
+  return (
+    <StyledFooter>
+      sarchoi ∙ minjupar ∙ yunjung <br /> ⓒ 42 EVENT
+    </StyledFooter>
+  );
 };
 
 export default React.memo(Footer);
 
 const StyledFooter = styled.footer`
-  height: 52px;
+  height: 64px;
   padding: 14px;
   font-size: 0.8rem;
+  line-height: 1.1rem;
   text-align: center;
   border-top: 1px solid var(--darksnow);
   color: var(--lightgray);

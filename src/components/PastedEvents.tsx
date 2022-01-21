d8e484eee56ddaaf57426b4a279a0682ad4ce6af
6mn12j <dbwk0404@hanmail.net>
2022-01-22 05:10:45 +0900

    MTUP-82 무한스크롤 뼈대 작업

--- src/components/PastedEvents.tsx
+++ src/components/PastedEvents.tsx
@@ -3,32 +3,26 @@ import { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { getEventsPagination } from '../api/api';
 import { Event } from '../types/event';
-//import { usePageDispatch, usePageState } from '../contexts/PageContext';
-import EventCard from './EventCard';
+import EventList from './EventList';
 
 const PastedEvents = () => {
-  //const { page } = usePageState();
   const [page, setPage] = useState(1);
-  const [lastPage, setLastPage] = useState(false);
-  //const dispatch = usePageDispatch();
 
-  const { ref, inView } = useInView({ skip: lastPage });
+  const { ref, inView } = useInView();
   const [loading, setLoading] = useState(false);
   const [pastedEvents, setPastedEvents] = useState<Event[] | []>([]);
 
   const getEvents = useCallback(async () => {
     setLoading(true);
     const response = await getEventsPagination('past', page);
-    if (response.data.length !== 0) setPastedEvents(response.data);
-    else {
-      setLastPage(true);
-    }
+    const event: Event[] = response.data;
+    if (event.length !== 0)
+      setPastedEvents((prevState) => [...prevState, ...event]);
     setLoading(false);
   }, [page]);
 
   useEffect(() => {
     if (inView && !loading) {
-      //dispatch({ type: 'ADD_PAGE' });
       setPage((prevState) => prevState + 1);
     }
   }, [inView, loading]);
@@ -38,12 +32,10 @@ const PastedEvents = () => {
   }, [getEvents]);
 
   return (
-    <div>
-      {pastedEvents.map((event) => {
-        return <EventCard event={event} />;
-      })}
-      <div ref={ref}>Element {inView.toString()}</div>
-    </div>
+    <>
+      <EventList events={pastedEvents} />
+      <div ref={ref} />
+    </>
   );
 };
 

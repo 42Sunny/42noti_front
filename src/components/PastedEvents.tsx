1d5255bc9c11058fdaa6f9d02796788335ec5556
6mn12j <dbwk0404@hanmail.net>
2022-01-24 04:11:49 +0900

    MTUP-82 지나간 이벤트 API 예외처리
    데이터를 불러온 후 성공 했을때만 EventList컴포넌트에 들어가게 예외처리를
    하였습니다.

--- src/components/PastedEvents.tsx
+++ src/components/PastedEvents.tsx
@@ -1,8 +1,8 @@
-import React, { useCallback, useState } from 'react';
-import { useEffect } from 'react';
+import React, { useCallback, useRef, useState, useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 import styled from 'styled-components';
 import { getEventsPagination } from '../api/api';
+import { StyledSection } from '../pages/NotFound';
 import { Event } from '../types/event';
 import EventList from './EventList';
 
@@ -12,10 +12,14 @@ const PastedEvents = () => {
   const [loading, setLoading] = useState(false);
   const [buttinIsvisible, setButtonIsvisible] = useState(true);
   const [pastedEvents, setPastedEvents] = useState<Event[] | []>([]);
+  const isSuccess = useRef<boolean | undefined>(undefined);
 
   const getEvents = useCallback(async () => {
     setLoading(true);
     const response = await getEventsPagination('past', page);
+    if (response.status === 200) isSuccess.current = true;
+    else isSuccess.current = false;
+
     const event: Event[] = response.data;
     if (event.length !== 0)
       setPastedEvents((prevState) => [...prevState, ...event]);
@@ -39,11 +43,16 @@ const PastedEvents = () => {
 
   return (
     <>
-      <EventList events={pastedEvents} />
-      {buttinIsvisible ? (
+      {isSuccess.current === true && <EventList events={pastedEvents} />}
+      {buttinIsvisible && isSuccess.current === true ? (
         <Button onClick={toggleButtonIsVisible}>더보기</Button>
       ) : (
         <div ref={ref} />
+      )}
+      {isSuccess.current === false && (
+        <StyledSection>
+          <h1>데이터를 불러오는데 실패 했습니다.</h1>
+        </StyledSection>
       )}
     </>
   );
@@ -53,10 +62,12 @@ export default PastedEvents;
 
 const Button = styled.button`
   all: unset;
-  width: 90px;
-  height: 45px;
+  width: 80px;
+  height: 40px;
   text-align: center;
   background-color: var(--blue);
   color: var(--white);
+  font-weight: bold;
   border-radius: 20px;
+  cursor: pointer;
 `;

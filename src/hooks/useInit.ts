a3dbc08d66065b789d9c8750580c87b242d821f1
6mn12j <dbwk0404@hanmail.net>
2022-01-31 00:06:04 +0900

    MTUP-83 gitignore 파일 hooks 삭제

---
+++ src/hooks/useInit.ts
@@ -0,0 +1,25 @@
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+import { useEffect } from 'react';
+
+const initSentry = () => {
+  if (process.env.REACT_APP_ENV !== 'local') {
+    Sentry.init({
+      dsn: process.env.REACT_APP_SENTRY_DSN,
+      integrations: [new Integrations.BrowserTracing()],
+      environment: process.env.REACT_APP_ENV,
+      // Set tracesSampleRate to 1.0 to capture 100%
+      // of transactions for performance monitoring.
+      // We recommend adjusting this value in production
+      tracesSampleRate: 1.0,
+    });
+  }
+};
+
+const useInit = () => {
+  useEffect(() => {
+    initSentry();
+  }, []);
+};
+
+export default useInit;

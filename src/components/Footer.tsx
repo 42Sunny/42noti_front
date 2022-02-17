72b923c3a29aead24d2458337b66c5d9df1a42e9
dopaming <over_ray@naver.com>
2022-02-18 03:38:09 +0900

    MTUP-65 푸터 깃헙 링크 연결

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,18 @@ import styled from 'styled-components';
 const Footer = () => {
   return (
     <StyledFooter>
-      sarchoi ∙ minjupar ∙ yunjung <br /> ⓒ 42 EVENT
+      <a href="https://github.com/srngch" rel="noreferrer" target="_blank">
+        sarchoi
+      </a>
+      ∙
+      <a href="https://github.com/6mn12j" rel="noreferrer" target="_blank">
+        minjupar
+      </a>
+      ∙
+      <a href="https://github.com/dopamingo" rel="noreferrer" target="_blank">
+        yunjung
+      </a>
+      <br /> ⓒ 42NOTI
     </StyledFooter>
   );
 };
@@ -20,4 +31,11 @@ const StyledFooter = styled.footer`
   border-top: 1px solid ${({ theme }) => theme.colors.darksnow};
   color: ${({ theme }) => theme.colors.lightgray};
   background: ${({ theme }) => theme.colors.snow};
+  a {
+    margin: 0 4px;
+    color: ${({ theme }) => theme.colors.lightgray};
+  }
+  a:hover {
+    color: ${({ theme }) => theme.colors.blue};
+  }
 `;
